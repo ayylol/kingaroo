bc6c5d42766197afc6bc94d39e2d70068be4d922
David Garcia Orozco <David.GarOro@protonmail.com>
2022-04-29 20:06:55 -0600

    Map generation algorithm now implemented

---
+++ world/Room.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+export var left_door := false
+export var right_door := false
+export var up_door := false
+export var down_door := false
+
+func _ready():
+	print("ROOM READY!")
